28160db9c2096774957bfb6fae736f8e7c368a6d
2red123123 <nigel4312@gmail.com>
2024-12-19 14:16:14 -0800

    main.py now counts all alphabetical characters

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    char_count = get_char_count(text)
     print(f"{num_words} words found in this document")
+    print(char_count)
 
+def get_char_count(text):
+    letter_count = {}
+    lower_char = text.lower()
+    for l in lower_char:
+        if l.isalpha():
+            if l in letter_count:
+                letter_count[l] += 1
+            else:
+                letter_count[l] = 1
+    return letter_count
 
 def get_num_words(book_text):
     words = book_text.split()
