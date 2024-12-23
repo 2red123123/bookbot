8aaea224609b1b06bfa1b378b000bae9d5a1b679
2red123123 <nigel4312@gmail.com>
2024-12-22 20:21:43 -0800

    main.py compiles a report based on target book

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = get_char_count(text)
+    char_list = get_dict_list(char_count)
+    char_list.sort(reverse = True, key = sort_on)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print()
     print(f"{num_words} words found in this document")
-    print(char_count)
+    print()
+    for dict in char_list:
+        print(f"The '{dict["name"]}' character was found {dict["num"]} times")
+    print("--- End Report ---")
+   
+def sort_on(char_count):
+    return char_count["num"]
+
+def get_dict_list(char_count):
+    list_of_char = []
+    for key, value in char_count.items():
+        list_of_char.append({"name": key, "num": value})
+    return list_of_char
 
 def get_char_count(text):
     letter_count = {}
@@ -21,10 +38,8 @@ def get_num_words(book_text):
     words = book_text.split()
     return len(words)
 
-
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
-
 main()
